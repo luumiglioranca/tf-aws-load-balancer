d465f3460feb79d1b0a3528adf3d7b2293c1bebf
Lucas Migliorança <41163225+luumiglioranca@users.noreply.github.com>
2024-04-10 10:59:55 -0300

    Update route_53.tf

--- load-balancer-all-resources/module/route_53.tf
+++ load-balancer-all-resources/module/route_53.tf
@@ -4,17 +4,17 @@
 #                                                                           #
 #############################################################################
 
-data "aws_route53_zone" "edtech" {
-  provider     = aws.atena
+data "aws_route53_zone" "dns_zone" {
+  provider     = aws.sua_conta
   name         = local.domain_name
   private_zone = "false"
 }
 
 resource "aws_route53_record" "application_load_balancer_dns" {
   provider = aws.atena
-  zone_id  = data.aws_route53_zone.edtech.zone_id
+  zone_id = data.aws_route53_zone.dns_zone.zone_id
 
-  name    = "${local.alb_name}.${local.domain_name}"
+  name    = "${local.dns_name}.${local.domain_name}"
   type    = "CNAME"
   ttl     = "10"
   records = [module.application_load_balancer.dns_name]
